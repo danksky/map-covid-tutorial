b4c8b2383310963f79a0d3eeb5870bd1807dfe68
Daniel Kawalsky <daniel.kawalsky@gmail.com>
2020-04-14 22:22:12 -0500

    added d3 json to Map component with just console log

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import '../stylesheets/Map.css';
+import * as d3 from 'd3';
 
 export default class Map extends React.Component {
   constructor(props) {
@@ -9,12 +9,16 @@ export default class Map extends React.Component {
   }
 
   componentDidMount() {
-
+    d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json")
+      .then(json => {
+        console.log(json);
+      })
   }
 
   render() {
     return (
       <div className="Map" >
+        MAPONENT
       </div>
     );
   }
