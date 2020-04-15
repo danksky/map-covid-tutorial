aa4d93e32e62be44c148f441f343bb0a8ef995ff
Daniel Kawalsky <daniel.kawalsky@gmail.com>
2020-04-14 21:00:00 -0500

    added Map.jsx, Map.css, and directory structure

---
+++ src/components/Map.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../stylesheets/Map.css';
+
+export default class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="Map" >
+      </div>
+    );
+  }
+}
